10a38369521855910fbe91ac07c21d47fada32f7
Will Ross <WillPRoss@gmail.com>
2023-01-27 19:00:56 +0000

    creating second version of hexmap for comparison

--- app/controllers/hex_system_controller.rb
+++ app/controllers/hex_system_controller.rb
@@ -1,22 +1,35 @@
 class HexSystemController < ApplicationController
-    
-    def system_details
-        @data = System.translated_system_info
-    end
-    
-    def hexmap
-        raw_hexmap_data = System.hexmap
 
-        organized_data = raw_hexmap_data.map {|point| {n: point.name, q: point.q, r: point.r, hex_loc: point.location, colour:"rgb(247, 171, 45)"}}
-        raw_json_data = organized_data.as_json.to_json
-        parsed_data = JSON.parse(raw_json_data)
-        formatted_data = {}
-        parsed_data.each_with_index do |item, index|
-            formatted_data[index] = item
-        end
-        
-        gon.data = formatted_data
-        
-    end
-    
+	def system_details
+		@data = System.translated_system_info
+	end
+
+	def hexmap
+		raw_hexmap_data = System.hexmap
+
+		organized_data = raw_hexmap_data.map {|point| {n: point.name, q: point.q, r: point.r, hex_loc: point.location, colour:"rgb(247, 171, 45)"}}
+		raw_json_data = organized_data.as_json.to_json
+		parsed_data = JSON.parse(raw_json_data)
+		formatted_data = {}
+		parsed_data.each_with_index do |item, index|
+				formatted_data[index] = item
+		end
+
+		gon.data = formatted_data
+	end
+
+	def hextest
+		raw_hexmap_data = System.hexmap
+
+		organized_data = raw_hexmap_data.map {|point| {n: point.name, q: point.q, r: point.r, hex_loc: point.location, colour:"rgb(247, 171, 45)"}}
+		raw_json_data = organized_data.as_json.to_json
+		parsed_data = JSON.parse(raw_json_data)
+		formatted_data = {}
+		parsed_data.each_with_index do |item, index|
+				formatted_data[index] = item
+		end
+
+		gon.data = formatted_data
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
     collection do
       get 'system_details'
       get 'hexmap'
+      get 'hextest'
     end
   end
 
